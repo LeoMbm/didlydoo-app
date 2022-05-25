d697c0c847b2cd591a4c1ff6386fa7d546fd6256
Leo Mbm <leonidas.j.mbm@gmail.com>
2022-05-25 12:51:11 +0200

    modal done

--- src/js/eventCase/createEvent.js
+++ src/js/eventCase/createEvent.js
@@ -1,8 +1,21 @@
-async function addEvents() {
+const title = document.getElementById("#input-name");
+const dates = document.getElementById("#input-date");
+const description = document.getElementById("#input-desc");
+
+const button = document.querySelector("#submit-btn");
+
+export default async function addEvents() {
   const req = await fetch(
     "http://localhost:3000/api/events/"({
       method: "POST",
-      body: [{ name: "", dates: [""], author: "", description: "" }],
+      body: [
+        {
+          name: title.value,
+          dates: [dates.values],
+          author: title.value,
+          description: description.value,
+        },
+      ],
     })
   );
 }
--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,17 @@ import openModalCreate from "./modal/openModalCreate.js";
 import closeModal from "./modal/closeModal.js";
 import getEvents from "./eventCase/getEvents.js";
 import "./modal/calendar.js";
+import addEvents from "./eventCase/createEvent.js";
 
 // QUERY SELECTOR
 let btnCreate = document.querySelector("#btn-create");
 let btnClose = document.querySelector("#modal-close");
+let btnSubmit = document.querySelector("#submit-btn");
 
 // LISTENERS
 btnCreate.addEventListener("click", openModalCreate);
 btnClose.addEventListener("click", closeModal);
+btnSubmit.addEventListener("click", addEvents);
 // FUNCTION
 
 getEvents();
